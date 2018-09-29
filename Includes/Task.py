b461aa91e68189cf02a014eb0f7cfb5644cf3921
Natasha Ghantasala <natasha.ghantasala@gmail.com>
2018-09-29 12:21:03 -0700

    Adding the first task file

---
+++ Includes/Task.py
@@ -0,0 +1,20 @@
+
+
+
+class Task(object):
+
+    def __init__(self):
+        #Declare all the properties
+        self.DueDate = None
+        self.Project = None
+        self.Assignee = None
+        self.Owner = None
+        self.Description = None
+        self.Category = None
+        self.Comments = None
+        self.ParentTask = None
+        self.TaskName = None
+        self.uniqueID = None
+
+    #Declare all the methods that will perform operations on Task
+
